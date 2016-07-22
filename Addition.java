439b52d3deb5b3ddf7b67923a73416b6f27a3a18
swapna55 <swapnaragavan@gmail.com>
2016-07-23 00:25:52 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Addition
+{
+public static void main(String args[])
+{
+int num,a,b,c;
+Scanner s = new Scanner(System.in);
+System.out.println("enter the a and b values");
+a=s.nextInt();
+b=s.nextInt();
+c=a+b;
+System.out.println(+c);
+}
+}
