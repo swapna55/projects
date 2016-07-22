548eab04bdb5f93848480e9cbe9e18753b69dc1f
swapna55 <swapnaragavan@gmail.com>
2016-07-23 00:26:21 +0530

    Add files via upload

---
+++ Comparison.java
@@ -0,0 +1,19 @@
+public class Comparison
+{
+public static void main(String[] args)
+{
+int x=6;
+if(x>7)
+{
+System.out.println("x is greater than 7");
+}
+else if(x<3)
+{
+System.out.println("x is less than 3");
+}
+else
+{
+System.out.println("x is greater than 3");
+}
+}
+}
