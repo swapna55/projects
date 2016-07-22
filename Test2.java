b611ffb8f6bb5afbbadc2acf92feb04a2339d9bd
swapna55 <swapnaragavan@gmail.com>
2016-07-23 00:29:21 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,16 @@
+public class Test2
+{
+public static void main(String[] args)
+{
+int[] numbers={4,5,6,7,8};
+for(int x:numbers)
+{
+if(x==5)
+{
+continue;
+}
+System.out.println(x);
+System.out.println("\n");
+}
+}
+}
