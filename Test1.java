48d17fc54ce039a38b32eadd31105b38f930146f
swapna55 <swapnaragavan@gmail.com>
2016-07-23 00:28:57 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,16 @@
+public class Test1
+{
+public static void main(String[] args)
+{
+int[] numbers={4,5,6,7,8};
+for(int x:numbers)
+{
+if(x==5)
+{
+break;
+}
+System.out.println(x);
+System.out.println("\n");
+}
+}
+}
